1cf4b06d1e710aa126acfbe4bcedcf3801f76e4d
David Cruwys <david@ideasmen.com.au>
2021-09-17 23:17:17 +1000

    first commit

---
+++ lib/test/gpt3.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "gpt3/version"
+
+module Test
+  module Gpt3
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/test/gpt3_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Test::Gpt3 do
+  it "has a version number" do
+    expect(Test::Gpt3::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
